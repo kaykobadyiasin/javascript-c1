c569a9b16e4699dfb864bae121256055b17f42c6
Kaykobad Ahammed Yiasin <90813580+kaykobadyiasin@users.noreply.github.com>
2021-12-14 23:49:50 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// windows alert
+function windDows(){
+  window.alert('Make good use of this website')
+}
+// document write
+function conLog(){
+  console.log('Console Log')
+}
+
+// document write
+function docWrite(){
+  document.write('How are you?')
+}
+
+// alart
+function welcome(){
+  alert('Be careful');
+}
+// images
+function imgOne(){
+
+  document.getElementById('img').src = "images/js.jpg";
+}
+function imgTwo(){
+
+  document.getElementById('img').src = "images/js2.jpg";
+}
+// Text
+function textChange(){
+
+  document.getElementById('p').innerHTML = "Welcome to bangladesh";
+}
+
+// Text number
+function textMultiChange(){
+
+  document.getElementsByClassName('n')[2].innerHTML = "Welcome to bangladesh";
+}
+
